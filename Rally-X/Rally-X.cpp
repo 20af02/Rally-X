dc578431b38c7f40a9aeeebd939af987e58cd688
20af02 <47429824+20af02@users.noreply.github.com>
2020-02-03 17:47:52 -0600

    Initial Setup
    SFML initialization

---
+++ Rally-X/Rally-X.cpp
@@ -0,0 +1,21 @@
+/************************************************
+Authors:
+Ammaar Firozi, Nathanael Hagemeyer,
+Jack Schilling, Adin Aberbach.
+
+Project Goal:
+Recreate the classic arcade game Rally-X in C++
+with SFML.
+
+************************************************/
+
+#include <iostream>
+
+
+int main()
+{
+
+
+    return 0;
+}
+
