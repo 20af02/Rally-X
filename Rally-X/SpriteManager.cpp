a6affe8dbb4c4c66a05177909227f2ad62a237bc
20af02 <47429824+20af02@users.noreply.github.com>
2020-02-28 08:57:44 -0600

    Sprite Manager
    adedd base Sprite Manager

--- Rally-X/SpriteManager.cpp
+++ Rally-X/SpriteManager.cpp
@@ -1,1 +1,22 @@
 #include "SpriteManager.h"
+
+SpriteManager::SpriteManager(std::string sheetFile)
+{
+	spriteSheet.loadFromFile(sheetFile);
+}
+
+
+SpriteManager::~SpriteManager()
+{
+}
+
+
+void SpriteManager::setTexture(sf::Sprite& sprite, SheetSection section, int x, int y)
+{
+	int startX = startCoordinates[section][0] + gridSizes[section][0] * x;
+	int startY = startCoordinates[section][1] + gridSizes[section][1] * y;
+
+	sprite.setTexture(spriteSheet);
+	//sprite.setTextureRect(sf::IntRect(startX, startY, endX, endY));
+	sprite.setTextureRect(sf::IntRect(startX, startY, gridSizes[section][0], gridSizes[section][1]));
+}
--- Rally-X/SpriteManager.h
+++ Rally-X/SpriteManager.h
@@ -1,5 +1,21 @@
-#pragma once
+#ifndef SPRITEMANAGER_H
+#define SPRITEMANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+
 class SpriteManager
 {
+public:
+	SpriteManager(std::string);
+	~SpriteManager();
+	enum SheetSection { Cars, MapTiles, Obstacles, Flags, Life };
+	void setTexture(sf::Sprite&, SheetSection, int, int);
+private:
+	sf::Texture spriteSheet;
+	int startCoordinates[5][2] = { {0, 0}, {0, 112}, {0, 80}, {1, 65}, {195, 65} };
+	int gridSizes[5][2]{ {16, 16}, {24, 24}, {24, 24}, {15, 11}, {10, 10} };
 };
 
+#endif // !SPRITEMANAGER_H
