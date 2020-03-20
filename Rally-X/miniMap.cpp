222e8078282aea9b85b65b39d698de1e2fdac0b5
Drag0n-Spirit <47537982+Drag0n-Spirit@users.noreply.github.com>
2020-03-20 15:17:23 -0500

    Finished minimap - needs testing
    Made some assumptions here that should be checked. If the map size can vary I'll need to adjust those hard coded values.
    Also requires all flags and enemies be initialized, but checks if they are active before trying to draw them on the minimap.

--- Rally-X/miniMap.cpp
+++ Rally-X/miniMap.cpp
@@ -8,15 +8,22 @@ miniMap::~miniMap()
 {
 }
 
+
+//Requires all 8 enemy cars, all 10 flags, and player to be initialized.
+//Assumes map size is 64 tiles of 24 pixels each and sprites are drawn from the top left corner.
 void miniMap::draw(sf::RenderTarget& target) const
 {
 	sf::Vector2f pos;
+	//These values are used a lot. Storing them costs 2 floats of memory 
+	//but significantly reduces our pointer calls
+	float screenWidth = graphicsSettings_->resolution.width;
+	float screenHeight = graphicsSettings_->resolution.height;
 
 	//Background blue screen for radar minimap
-	sf::RectangleShape background(sf::Vector2f(graphicsSettings_->resolution.width * 2.f / 9.f, graphicsSettings_->resolution.height));
+	sf::RectangleShape background(sf::Vector2f(screenWidth * 2.f / 9.f, screenHeight / 2));
 	sf::RectangleShape item(sf::Vector2f(4, 4));
 	background.setFillColor(sf::Color(33, 71, 151));
-	background.setPosition(sf::Vector2f(graphicsSettings_->resolution.width * 7.f / 9.f, graphicsSettings_->resolution.height / 2));
+	background.setPosition(sf::Vector2f(screenWidth * 7.f / 9.f, screenHeight * 0.3));
 
 
 	target.draw(background);
@@ -30,12 +37,18 @@ void miniMap::draw(sf::RenderTarget& target) const
 	for (int i = 0; i < 8; i++)
 	{
 		/*
-		if (this->objMan_->getObject(enemy, i).isActive())
+		if (this->objMan_->getObject(enemy, i).getState() == 0)
 		{
 		pos = this->objMan_->getObject(enemy, i).getPosition();
 		//Apply scale factor
+		//Map is 64 * 24 pixels by 42 * 24 pixels (before zooming). Take this and use as a ratio
+		pos.x = (screenWidth * 2/9) * (pos.x / (64 * 24));
+		pos.y = (screenHeight * 0.5) * (pos.y / (64 * 24));
+		pos.x += screenWidth * 7/9; //Offset to account for HUD position
+		pos.y += screenHeight * 0.3; //Offset to account for HUD position
 
 		//Draw square
+		item.setPosition(pos);
 		target.draw(item);
 		}
 		*/
@@ -47,12 +60,18 @@ void miniMap::draw(sf::RenderTarget& target) const
 	for (int i = 0; i < 10; i++)
 	{
 		/*
-		if (this->objMan_->getObject(flag, i).isActive())
+		if (this->objMan_->getObject(enemy, i).getState() == 0)
 		{
-		pos = this->objMan_->getObject(flag, i).getPosition();
+		pos = this->objMan_->getObject(enemy, i).getPosition();
 		//Apply scale factor
+		//Map is 64 * 24 pixels by 42 * 24 pixels (before zooming). Take this and use as a ratio
+		pos.x = (screenWidth * 2/9) * (pos.x / (64 * 24));
+		pos.y = (screenHeight * 0.5) * (pos.y / (64 * 24));
+		pos.x += screenWidth * 7/9; //Offset to account for HUD position
+		pos.y += screenHeight * 0.3; //Offset to account for HUD position
 
 		//Draw square
+		item.setPosition(pos));
 		target.draw(item);
 		}
 		*/
@@ -62,9 +81,15 @@ void miniMap::draw(sf::RenderTarget& target) const
 	item.setFillColor(sf::Color::Blue);
 	pos = this->objMan_->getObject(player, 0).getPosition();
 	//Apply scale factor
+	//Map is 64 * 24 pixels by 42 * 24 pixels (before zooming). Take this and use as a ratio
+	pos.x = (screenWidth * 2 / 9) * (pos.x / (64 * 24));
+	pos.y = (screenHeight * 0.5) * (pos.y / (64 * 24));
+	pos.x += screenWidth * 7 / 9; //Offset to account for HUD position
+	pos.y += screenHeight * 0.3; //Offset to account for HUD position
 
 	//Draw square
-
+	item.setPosition(pos);
+	target.draw(item);
 
 	return;
 }
